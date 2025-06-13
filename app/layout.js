e8c508a8c4bcf348dffae5489256da7672e816d5
René DeAnda <rene.ideanda@gmail.com>
2025-06-12 19:53:41 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,7 @@ export const metadata = {
   },
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
